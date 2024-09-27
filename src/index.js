907c65cfe1519e5c88c18866f2602b55830b285f
Syed Hussain Ather <shussainather@gmail.com>
2024-09-27 02:25:28 -0400

    serviceworker

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+serviceWorkerRegistration.register();
 
